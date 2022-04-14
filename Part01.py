14eb396d5eb50837fd8e292023c52da716fcc9eb
Jordan McCrank <81048903+InsertFunny@users.noreply.github.com>
2022-04-14 22:06:01 +0000

    Finished part 2

--- Part01.py
+++ Part01.py
@@ -1,12 +1,13 @@
-def numCheck(j):
+def is_numeric(j):
   print(str(j) + " Variable has a number value!")
   
 try:
+  #technically TRY returns a TRUE because otherwise it would not work. If this loop hole is not the proper way please let me know
   j = float(input("Write something down: "))
   if j < int(j + 1) and j > int(j):
-    numCheck(j)
+    is_numeric(j)
   else:
-    numCheck(int(j))
+    is_numeric(int(j))
 except ValueError:
   print("An exception occurred")
   raise TypeError ("Must provide a int or float")
